d904f73febaa7587e4838e7b751bf82eb2f26dd0
krachzack <hello@phstadler.com>
2019-03-05 19:50:05 +0100

    Added bell not installed error

--- fernspielapparat/src/act/bell.rs
+++ fernspielapparat/src/act/bell.rs
@@ -1,3 +1,4 @@
+use err::BellError;
 use failure::Error;
 use crate::act::Act;
 
@@ -5,10 +6,27 @@ pub struct Bell;
 
 impl Act for Bell {
     fn done(&self) -> Result<bool, Error> {
-        unimplemented!()
+        Err(BellError::not_installed())
     }
 
     fn cancel(&mut self) -> Result<(), Error> {
-        unimplemented!()
+        Err(BellError::not_installed())
+    }
+}
+
+
+mod err {
+    use failure::{Fail, Error};
+
+    #[derive(Fail, Debug)]
+    pub enum BellError {
+        #[fail(display = "The system has no bell installed")]
+        NotInstalled
+    }
+
+    impl BellError {
+        pub fn not_installed() -> Error {
+            From::from(BellError::NotInstalled)
+        }
     }
 }
