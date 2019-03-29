1ea990eb8d7a46efdc8ccd317e25f3c06a989b7f
krachzack <hello@phstadler.com>
2019-03-29 23:14:27 +0100

    Using bounded channels, propagating fatal sensor errors

--- fernspielapparat/src/sense/bg.rs
+++ fernspielapparat/src/sense/bg.rs
@@ -1,19 +1,20 @@
-use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
 use std::thread;
 use crate::sense::{Sense, Error, dial::Input};
 
-pub struct BackgroundSense(Receiver<Input>);
+pub struct BackgroundSense(Receiver<Result<Input, Error>>);
 
 impl Sense for BackgroundSense {
     fn poll(&mut self) -> Result<Input, Error> {
         self.0.try_recv()
-            .or_else(|_| Err(Error::WouldBlock))
+            .unwrap_or(Err(Error::WouldBlock))
     }
 }
 
 impl BackgroundSense {
     pub fn spawn(sense: Box<dyn Sense + Send>) -> Box<dyn Sense> {
-        let (tx, rx) = channel();
+        // 1: if last input has not been handled yet, let worker block until receiver polled
+        let (tx, rx) = sync_channel(1);
         thread::spawn(move || {
             keep_polling(sense, tx);
         });
@@ -21,12 +22,16 @@ impl BackgroundSense {
     }
 }
 
-fn keep_polling(mut sense: Box<dyn Sense>, sender: Sender<Input>) {
+fn keep_polling(mut sense: Box<dyn Sense>, sender: SyncSender<Result<Input, Error>>) {
     loop {
         match sense.poll() {
-            Ok(input) => sender.send(input).expect("Could not send input back"),
-            Err(Error::Fatal(e)) => break,
-            Err(Error::WouldBlock) => thread::yield_now()
+            Ok(input) => sender.send(Ok(input))
+                .expect("Could not send input back"),
+            Err(Error::WouldBlock) => thread::yield_now(),
+            fatal => {
+                sender.send(fatal);
+                break;
+            },
         }
     }
 }
