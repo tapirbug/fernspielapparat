1380b97cf4eab57f66a8eb2780070bb302d8e11a
krachzack <hello@phstadler.com>
2019-07-03 19:37:43 +0200

    chore: updated build script

--- fernspielapparat/deploy.sh
+++ fernspielapparat/deploy.sh
@@ -2,8 +2,8 @@
 # Builds and deploys the runtime to Raspberry Pi 2
 
 # RPi2-compatible ARM
-RPI_USER="tom"
-RPI_HOSTNAME="phantom.local"
+RPI_USER="pi"
+RPI_HOSTNAME="rudi.local"
 RPI_DIR="~"
 SSH_HOST="$RPI_USER@$RPI_HOSTNAME"
 SCP_DEST="$SSH_HOST:$RPI_DIR"
