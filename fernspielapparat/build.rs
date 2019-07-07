f5d802ee3da77eeff05ecd3fdb685dad41b76007
krachzack <hello@phstadler.com>
2019-07-07 19:54:00 +0200

    chore: autoformatted

--- fernspielapparat/build.rs
+++ fernspielapparat/build.rs
@@ -1,6 +1,5 @@
-
 #[cfg(target_os = "macos")]
-const VLC_LIB_DIR : &str = "/Applications/VLC.app/Contents/MacOS/lib";
+const VLC_LIB_DIR: &str = "/Applications/VLC.app/Contents/MacOS/lib";
 
 fn main() {
     #[cfg(target_os = "macos")]
