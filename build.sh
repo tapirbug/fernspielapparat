d6561707fddc9c78a5ed0e2163a14d1793a836fd
krachzack <hello@phstadler.com>
2019-09-28 21:17:38 +0200

    feat: adjust build script for cross-compiling
    Make the build script accept a target triple for cross-compiling. Adjust script logic to copy the correct binary and choose a sensible name for the output directory.

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
-# Creates a distributable tarball for the local platform.
-# If a cross-compile toolchain is available, also builds for ARM.
+# Creates a distributable tarball for a local platform or for
+# a given target triple. To build for the local platform, do not
+# pass any arguments:
+#
+#     ./build.sh
+#
+# The result is a tarball with a name like `fernspielapparat-0.1.0-linux-x86_64.tar.gz`,
+# which is assembled from uname info.
+#
+# To cross-compile with an ARM toolchain to get a tarball like
+# `release/fernspielapparat-<version>-arm-unknown-linux-gnueabihf.tar.gz`,
+# invoke the script with the desired target triple:
+#
+#     ./build.sh arm-unknown-linux-gnueabihf
 
 METADATA=$(head -n 4 Cargo.toml | sed -n 's/^.*"\([^"]*\)".*$/\1/p')
 CRATE=$(echo "$METADATA" | head -n 1)
@@ -8,9 +20,23 @@ VERSION=$(echo "$METADATA" | sed -n '2p')
 
 HOST_SYSTEM_NAME=$(uname | sed -e 's/\(.*\)/\L\1/')    # lower-case name like "linux"
 HOST_SYSTEM_ARCH=$(uname -m | sed -e 's/\(.*\)/\L\1/') # lower-case, e.g. "x86_64"
-RELEASE_DIR_NAME="$CRATE-$VERSION"
+
+CARGO_ARGS="--release"
+if [ -z "$1" ]
+then
+    echo "Building for host system architecture ..."
+    BINARY="target/release/$CRATE"
+    RELEASE_DIR_NAME="$CRATE-$VERSION-$HOST_SYSTEM_NAME-$HOST_SYSTEM_ARCH"
+else
+    CROSS_TRIPLE="$1"
+    echo "Cross-compiling for achitecture $CROSS_TRIPLE ..."
+    CARGO_ARGS="$CARGO_ARGS --target=$CROSS_TRIPLE"
+    BINARY="target/$CROSS_TRIPLE/release/$CRATE"
+    RELEASE_DIR_NAME="$CRATE-$VERSION-$CROSS_TRIPLE"
+fi
+
 RELEASE_DIR="release/$RELEASE_DIR_NAME"
-RELEASE_TAR="$RELEASE_DIR_NAME-$HOST_SYSTEM_NAME-$HOST_SYSTEM_ARCH.tar.gz" # fernspielapparat-0.1.0-linux-x86_64.tar.gz
+RELEASE_TAR="$RELEASE_DIR_NAME.tar.gz" # fernspielapparat-0.1.0-linux-x86_64.tar.gz
 
 echo "Building $CRATE into $RELEASE_DIR"
 
@@ -33,12 +59,11 @@ function generate_source_link {
 https://github.com/krachzack/fernspielapparat" > $RELEASE_DIR/SOURCE
 }
 
-echo "Building for host system architecture ..." && \
-cargo build --release && \
+cargo build $CARGO_ARGS && \
 echo "Clearing output directory ..." && \
 clean && \
 echo "Copying binary ..." && \
-cp target/release/$CRATE $RELEASE_DIR || cp target/release/$CRATE.exe $RELEASE_DIR && \
+cp $BINARY $RELEASE_DIR || cp $BINARY.exe $RELEASE_DIR && \
 echo "Copying static assets ..." && \
 copy_assets && \
 generate_source_link && \
