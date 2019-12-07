d691f5b5615cb10207ba814870a6d21bab8a4e29
krachzack <hello@phstadler.com>
2019-12-07 20:41:03 +0100

    test: add test coverage for #18
    Added a test that verifies that the runtime behaves correctly for the issue described in issue #18. Turns out there was a timeout after 0s that only made it appear like the symbol is handled twice, while actually the systems behaves to spec.

--- tests/remote_control.rs
+++ tests/remote_control.rs
@@ -20,6 +20,14 @@ const DIAL_ONE: &str = "{
     \"invoke\": \"dial\",
     \"with\": \"1\"
 }";
+const PICK_UP: &str = "{
+    \"invoke\": \"dial\",
+    \"with\": \"p\"
+}";
+const HANG_UP: &str = "{
+    \"invoke\": \"dial\",
+    \"with\": \"h\"
+}";
 
 const START_ON_PASSIVE_EVT: &str = "---
 type: start
@@ -200,7 +208,113 @@ fn observe_transition_from_dial() {
     );
 }
 
+#[test]
+fn avoid_double_transition() {
+    fernspielapparat::log::init_logging(Some(3));
+
+    // given: a phonebook where a transition criterion is also a transition criterion on the target state
+    let phonebook: &str = include_str!("../test/tv.yaml");
+    let port = random_port();
+
+    // when: picking up and then dialing one three times, then hanging up
+    let mut app = fernspielapparat::App::builder();
+    app.startup_phonebook(fernspielapparat::books::from_str(phonebook).unwrap());
+    app.serve(&format!("127.0.0.1:{port}", port = port))
+        .unwrap();
+    app.exit_on_terminal_state();
+    spawn(move || {
+        let mut app = app.build().unwrap();
+        app.run().unwrap();
+    });
+    let client = ClientBuilder::new(&format!("ws://127.0.0.1:{port}/", port = port))
+        .unwrap()
+        .add_protocol("fernspielctl")
+        .connect_insecure()
+        .expect("failed to make ws connection");
+    let (mut rx, mut tx) = client.split().unwrap();
+
+    let mut incoming = rx.incoming_messages();
+    let _event_start_ring = incoming
+        .next()
+        .expect("expected message of starting at the initial state")
+        .expect("expected ok message");
+    tx.send_message(&OwnedMessage::Text(PICK_UP.to_string()))
+        .unwrap();
+    let event_transition_to_introduce = incoming
+        .next()
+        .expect("expected message of transition after pick up")
+        .expect("expected ok message");
+    tx.send_message(&OwnedMessage::Text(DIAL_ONE.to_string()))
+        .unwrap();
+    let event_transition_to_talk = incoming
+        .next()
+        .expect("expected message of transition after dialing one the first time")
+        .expect("expected ok message");
+    tx.send_message(&OwnedMessage::Text(DIAL_ONE.to_string()))
+        .unwrap();
+    let event_transition_to_quiet = incoming
+        .next()
+        .expect("expected message of transition after dialing one the second time")
+        .expect("expected ok message");
+    tx.send_message(&OwnedMessage::Text(DIAL_ONE.to_string()))
+        .unwrap();
+    std::thread::sleep(std::time::Duration::from_secs(2));
+    let event_transition_to_talk_second_time = incoming
+        .next()
+        .expect("expected message of transition after dialing one the third time")
+        .expect("expected ok message");
+    tx.send_message(&OwnedMessage::Text(HANG_UP.to_string()))
+        .unwrap();
+    let event_transition_to_pause = incoming
+        .next()
+        .expect("expected message of transition after dialing one the second time")
+        .expect("expected ok message");
+
+    // then: there should be only two transitions triggered by the dialing of ones
+    assert_eq!(
+        event_transition_to_introduce,
+        dial_transition_evt_msg("pick up", "RING", "INTRODUCE"),
+        "Expecting picking up to result in transition to INTRODUCE"
+    );
+    assert_eq!(
+        event_transition_to_talk,
+        dial_transition_evt_msg("type 1", "INTRODUCE", "TALK"),
+        "Expecting first dial of one to result in transition to TALK"
+    );
+    assert_eq!(
+        event_transition_to_quiet,
+        dial_transition_evt_msg("type 1", "TALK", "QUIET"),
+        "Expecting second dial of one to result in transition to QUIET"
+    );
+    assert_eq!(
+        event_transition_to_talk_second_time,
+        dial_transition_evt_msg("type 1", "QUIET", "TALK"),
+        "Expecting third dial of one to result in transition back to QUIET"
+    );
+    assert_eq!(
+        event_transition_to_pause,
+        dial_transition_evt_msg("hang up", "TALK", "PAUSE"),
+        "Expecting hanging up to result in transition to PAUSE"
+    );
+}
+
 fn random_port() -> u32 {
     let rand: u32 = rand::random();
     10_000 + rand % 50_000
+}
+
+fn dial_transition_evt_msg(dial: &str, from: &str, to: &str) -> OwnedMessage {
+    OwnedMessage::Text(format!(
+        "---
+type: transition
+reason:
+  dial: {dial}
+from:
+  id: {from}
+to:
+  id: {to}",
+        dial = dial,
+        from = from,
+        to = to
+    ))
 }
